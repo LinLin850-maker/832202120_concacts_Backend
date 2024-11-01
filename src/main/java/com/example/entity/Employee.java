3a5d15caf009937c2345248a2de644c7606a2f63
LinLin850-maker <2010150889@qq.com>
2024-11-01 18:12:35 +0800

    Add files via upload

---
+++ src/main/java/com/example/entity/Employee.java
@@ -0,0 +1,49 @@
+package com.example.entity;
+
+public class Employee {
+    private Integer id;
+    private String sex;
+    private String descr;
+    private String name;
+    private Integer phoneNumber;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,52 @@
+package com.example.service;
+
+import com.example.entity.Employee;
+import com.example.mapper.EmployeeMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Resource
+    private EmployeeMapper employeeMapper;
+
+    public void add(Employee employee) {
+        employeeMapper.insert(employee);
+    }
+
+    public void update(Employee employee) {
+        employeeMapper.updateById(employee);
+    }
+
+    public void deleteById(Integer id) {
+        employeeMapper.deleteById(id);
+    }
+
+    public void deleteBatch(List<Integer> ids) {
+        for (Integer id : ids) {
+            this.deleteById(id);
+        }
+    }
+
+    public List<Employee> selectAll(Employee employee) {
+        return employeeMapper.selectAll(employee);
+    }
+
+    public Employee selectById(Integer id) {
+        return employeeMapper.selectById(id);
+    }
+
+    public PageInfo<Employee> selectPage(Employee employee,Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<Employee> list = employeeMapper.selectAll(employee);
+        return PageInfo.of(list);
+    }
+
+
+
+}
